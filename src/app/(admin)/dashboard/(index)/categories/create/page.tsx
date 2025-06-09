83c2808311c1a0e4d780597ec358525c44fe616d
AndreaSuryatanaya <andreasemut@gmail.com>
2025-06-09 08:47:19 +0700

    feat: create & update category

---
+++ src/app/(admin)/dashboard/(index)/categories/create/page.tsx
@@ -0,0 +1,5 @@
+import FormCategory from "../../_components/form-category";
+
+export default function CreatePage() {
+    return <FormCategory />;
+}
---
+++ src/app/(admin)/dashboard/(index)/lib/action.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { getUser, lucia } from "@/lib/auth";
+import { ActionResult } from "@/types";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import prisma from "../../../../../../lib/prisma";
+
+export async function Logout(_: unknown, formData: FormData): Promise<ActionResult> {
+    console.log("logout");
+
+    const { session } = await getUser();
+
+    if (!session) {
+        return {
+            error: "Unauthorized",
+        };
+    }
+
+    await lucia.invalidateSession(session.id);
+
+    const sessionCookie = lucia.createBlankSessionCookie();
+
+    cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+
+    return redirect("/dashboard/sign-in");
+}
+
+export async function deleteCategory(_: unknown, formData: FormData, id: number): Promise<ActionResult> {
+    try {
+        await prisma.category.delete({
+            where: {
+                id,
+            },
+        });
+    } catch (error) {
+        console.log(error);
+        return {
+            error: "Failed to delete data",
+        };
+    }
+
+    return redirect("/dashboard/categories");
+}
